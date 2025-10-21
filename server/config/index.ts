2d2084637601a6a2e103fb385695322039dcdce1
Lukman Mahmud <mistamann49@gmail.com>
2025-10-21 12:18:09 +0100

    fixed config

--- server/config/index.ts
+++ server/config/index.ts
@@ -16,6 +16,10 @@ const config = {
   defaultResLimit: 20,
   defaultResOffset: 0,
   RESEND_API_KEY: process.env.RESEND_API_KEY!,
+  FLW_PUBLIC_KEY: process.env.FLW_PUBLIC_KEY!,
+  FLW_SECRET_KEY: process.env.FLW_SECRET_KEY!,
+  FLW_ENCRYPTION_KEY: process.env.FLW_ENCRYPTION_KEY!,
+  FLW_WEBHOOK_HASH: process.env.FLW_WEBHOOK_HASH!,
 };
 
 export default config;
